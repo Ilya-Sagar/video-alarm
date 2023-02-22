8349e40e4d1dcd42f983001bb92a7e8456388102
IlyaS <workspace144@gmail.com>
2023-02-22 17:53:03 +1200

    Hide timestamps from main.py

--- main.py
+++ main.py
@@ -5,29 +5,15 @@ import time
 
 # 480 x 640
 
-# while True:
-#
-# # Делаем снимок
-#     ret, frame = cam.read()
-#
-#     # Записываем в файл
-#     cv2.imshow("cameraFeed", frame)
-#     ch = cv2.waitKey(5)
-#     if ch == 27:
-#         break
-#
-# # Отключаем камеру
-# cam.release()
-# cv2.destroyAllWindows()
-
-# exit()
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
+
 morph = Morph()
 morph.positive_value = 1
 morph.negative_value = 0
 morph.set_SE([[1] * 3] * 3, 1, 1)
+
 pivot = 10
 
 while True:
@@ -35,13 +21,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    start = time.time()
+    # start = time.time()
     cam = cv2.VideoCapture(0)
     ret1, frame1 = cam.read()
     time.sleep(0.01)
     ret2, frame2 = cam.read()
     cam.release()
-    print('two picture were made in', time.time() - start, 'seconds')
+    # print('two picture were made in', time.time() - start, 'seconds')
 
     start = time.time()
     width = len(frame1[0])
@@ -57,14 +43,14 @@ while True:
             value = abs((0.2125 * frame1[i][j][0]) + (0.7154 * frame1[i][j][1]) + (0.0721 * frame1[i][j][2]) - (0.2125 * frame2[i][j][0]) - (0.7154 * frame2[i][j][1]) - (0.0721 * frame2[i][j][2]))
             result_image[i].append(0 if value < pivot else 1)
 
-    print('monochrome picture was made in', time.time() - start, 'seconds')
-    start = time.time()
+    # print('monochrome picture was made in', time.time() - start, 'seconds')
+    # start = time.time()
 
     morph.set_binary_image(result_image)
     morph.opening(keep_sizes=True)
     result_image = morph.binary_image
-    print('picture was processed with MP in', time.time() - start, 'seconds')
-    start = time.time()
+    # print('picture was processed with MP in', time.time() - start, 'seconds')
+    # start = time.time()
 
     for i in range(480):
         for j in range(640):
@@ -73,7 +59,7 @@ while True:
 
 
     pygame.display.update()
-    print('picture was displayed in', time.time() - start, 'seconds', end='\n' + '-' * 10 + '\n')
+    # print('picture was displayed in', time.time() - start, 'seconds', end='\n' + '-' * 10 + '\n')
 
 # cam.release()
 
