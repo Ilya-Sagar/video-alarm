f3d62d18f3417dea83e7f37a1358965bc8f98393
IlyaS <workspace144@gmail.com>
2023-02-22 18:05:23 +1200

    Remove all pygame uses in main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import cv2
-import pygame
 from Morph import Morph
 import time
 
-# 480 x 640
-
-
-pygame.init()
-screen = pygame.display.set_mode((640, 480))
 
 morph = Morph()
 morph.positive_value = 1
@@ -17,50 +11,38 @@ morph.set_SE([[1] * 3] * 3, 1, 1)
 pivot = 10
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-    # start = time.time()
+    # make two frames
     cam = cv2.VideoCapture(0)
     ret1, frame1 = cam.read()
     time.sleep(0.01)
     ret2, frame2 = cam.read()
     cam.release()
-    # print('two picture were made in', time.time() - start, 'seconds')
 
-    start = time.time()
     width = len(frame1[0])
 
     frame1 = frame1.tolist()
     frame2 = frame2.tolist()
     result_image = []
 
-    # bring the image to a monochrome appearance
+    # bring the image to a monochrome appearance and subtract one from other
     for i in range(len(frame1)):
         result_image.append([])
         for j in range(width):
             value = abs((0.2125 * frame1[i][j][0]) + (0.7154 * frame1[i][j][1]) + (0.0721 * frame1[i][j][2]) - (0.2125 * frame2[i][j][0]) - (0.7154 * frame2[i][j][1]) - (0.0721 * frame2[i][j][2]))
             result_image[i].append(0 if value < pivot else 1)
 
-    # print('monochrome picture was made in', time.time() - start, 'seconds')
-    # start = time.time()
-
+    # apply morphological processing
     morph.set_binary_image(result_image)
     morph.opening(keep_sizes=True)
     result_image = morph.binary_image
-    # print('picture was processed with MP in', time.time() - start, 'seconds')
-    # start = time.time()
 
-    for i in range(480):
-        for j in range(640):
-            color = (0, 0, 0) if result_image[i][j] == 0 else (255, 255, 255)
-            screen.set_at((j, i), color)
+    # find square of result_image
+    square = 0
+    for i in range(len(result_image)):
+        for j in range(width):
+            if result_image[i][j] == 1:
+                square += 1
 
-
-    pygame.display.update()
-    # print('picture was displayed in', time.time() - start, 'seconds', end='\n' + '-' * 10 + '\n')
-
-# cam.release()
+    print(square)
 
 cv2.destroyAllWindows()
